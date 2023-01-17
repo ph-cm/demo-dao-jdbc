dcd87019e8f121e77c793d0936526b1f19908748
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2023-01-17 19:19:18 -0300

    SellerDaoJDBC and Dao Factory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Departament;
 import model.entities.Seller;
 
@@ -10,7 +12,12 @@ public class Program {
 	public static void main(String[] args) {
 		Departament obj= new Departament(1 ,"Books");
 		
+		
+		
 		Seller seller = new Seller(21 , "Pedro" , "Pedro@gmail.com" ,new Date() , 3000.0 , obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 	}
 }
