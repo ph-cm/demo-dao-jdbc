24ae2fddb9cb7899f5993bdf3c01764d30d24e31
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2023-01-17 18:53:51 -0300

    seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,16 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Departament;
+import model.entities.Seller;
+
 public class Program {
 
+	public static void main(String[] args) {
+		Departament obj= new Departament(1 ,"Books");
+		
+		Seller seller = new Seller(21 , "Pedro" , "Pedro@gmail.com" ,new Date() , 3000.0 , obj);
+		System.out.println(seller);
+	}
 }
