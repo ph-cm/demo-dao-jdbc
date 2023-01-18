4ff23afbdaf589cc32bd21a0c80e51ae4212c795
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2023-01-18 20:57:04 -0300

    findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,5 +21,10 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
