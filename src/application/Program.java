63e6bce4871afb9d181035916541dbd81b562871
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2023-01-18 23:11:13 -0300

    Delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -36,5 +39,10 @@ public class Program {
 		seller.setName("Jhon");
 		sellerDao.update(seller);
 		System.out.println("Updated");
+		
+		System.out.println("Digite um id para ser excluido: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deleted");
 	}
 }
